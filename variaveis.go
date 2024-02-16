e0700371b59743b92bc89208c34cffedc773feed
PedroParker <103612669+PedroParker@users.noreply.github.com>
2024-02-16 17:02:30 -0400

    Add files via upload

---
+++ variaveis.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main(){
+	var nome string = "Pedro Parker"
+	var idade int = 23
+	altura := 1.75
+
+	fmt.Println("Olá eu sou ", nome, "Tenho ", idade, "anos e tenho ", altura,"m de altura" )
+}
